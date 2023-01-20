5766a75ec7a5e5b605b7d81562e101509d679d4d
Artem <60568409+halp3ars@users.noreply.github.com>
2023-01-20 15:24:16 +0300

    ctrl + l

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -15,6 +15,22 @@ public class main {
         }
     }
 
+    public static void main(String[] args) {
+        main graph = new main(7); // v = размер графа
+
+        graph.addConnection(0, 1);
+        graph.addConnection(1, 5);
+        graph.addConnection(1, 3);
+        graph.addConnection(2, 1);
+        graph.addConnection(2, 7);
+        graph.addConnection(3, 1);
+        graph.addConnection(4, 5);
+        graph.addConnection(5, 1);
+        graph.addConnection(5, 6);
+        graph.addConnection(6, 5);
+
+        graph.shirina(0); // откуда стартмуем
+    }
 
     void shirina(int v, boolean visited[]) {
         LinkedList<Integer> queue = new LinkedList<>();
@@ -34,23 +50,6 @@ public class main {
 
     void shirina(int v) {
         shirina(v, new boolean[V]);
-    }
-
-    public static void main(String[] args) {
-        main graph = new main(7); // v = размер графа
-
-        graph.addConnection(0, 1);
-        graph.addConnection(1, 5);
-        graph.addConnection(1, 3);
-        graph.addConnection(2, 1);
-        graph.addConnection(2, 7);
-        graph.addConnection(3, 1);
-        graph.addConnection(4, 5);
-        graph.addConnection(5, 1);
-        graph.addConnection(5, 6);
-        graph.addConnection(6, 5);
-
-        graph.shirina(0); // откуда стартмуем
     }
 
     void addConnection(int v, int w) {
